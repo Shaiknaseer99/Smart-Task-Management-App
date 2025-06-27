c6c34746a4527764ae2cc6fccaa3313c36df04a9
Shaik Naseer <116249329+Shaiknaseer99@users.noreply.github.com>
2025-06-27 17:09:43 +0530

    Update Login.jsx

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -73,7 +73,7 @@ const Login = () => {
   };
 
   const handleGoogleLogin = () => {
-    window.location.href = 'http://localhost:5000/api/auth/google';
+    window.location.href = 'https://smart-task-management-app-idmu.onrender.com/api/auth/google';
   };
 
   return (
